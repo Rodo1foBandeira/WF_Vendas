8a2333b2754cc164a2b48bb5f1c495428b2f006b
Rodolfo de Almeida Bandeira <Rodolfo.Bandeira@terceirizadoraizen.com>
2023-08-07 17:53:40 -0300

    OpenAuthProviders não deve ir para dmnd01

--- WF_Vendas/Account/Login.aspx.cs
+++ WF_Vendas/Account/Login.aspx.cs
@@ -14,11 +14,16 @@ namespace WF_Vendas.Account
             RegisterHyperLink.NavigateUrl = "Register";
             OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
 
+            Console.WriteLine("Log 333"); // DMND02
+
             var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
             if (!String.IsNullOrEmpty(returnUrl))
             {
                 RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
             }
+            Console.WriteLine("Log 1111"); // DMND02
+
+            Console.WriteLine("Log 2222"); // DMND02
         }
     }
 }
